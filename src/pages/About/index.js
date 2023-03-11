dac479fbcec66827fb560f92ff579baa496f78d2
Md Rony <mdronyrsk01724@gmail.com>
2023-03-11 16:06:37 +0600

    design to do list app

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,9 +1,50 @@
 import React from "react";
+import TextField from "@mui/material/TextField";
+import { Box, Container } from "@mui/system";
+import { Button } from "@mui/material";
+import "./style.css";
 
 const About = () => {
   return (
     <>
-      <h1>helo rony</h1>
+      <Container>
+        <Box
+          display="flex"
+          flexDirection={"column"}
+          maxWidth={450}
+          margin="auto"
+          marginTop={"25px"}
+        >
+          <TextField
+            margin="normal"
+            type={"text"}
+            label="Full Name"
+            variant="standard"
+          />
+          <TextField
+            margin="normal"
+            type={"email"}
+            label="Email"
+            variant="standard"
+          />
+          <TextField
+            margin="normal"
+            type={"text"}
+            label="Designation"
+            variant="standard"
+            name="designation"
+          />
+          <TextField
+            margin="normal"
+            type={"date"}
+            variant="standard"
+            name="date"
+          />
+          <Button variant="contained" className="button">
+            Submit
+          </Button>
+        </Box>
+      </Container>
     </>
   );
 };
